39f796acc11ccc0a30604766edd1ad61b2c6abe8
NigelBynum <136828778+NigelBynum@users.noreply.github.com>
2023-08-16 16:30:52 -0400

    Add files via upload
    complete

---
+++ SpringBootResTfulDemoApplication.java
@@ -0,0 +1,13 @@
+//package rocks.zipcode.SpringBootRESTfulDemo;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class SpringBootResTfulDemoApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(SpringBootResTfulDemoApplication.class, args);
+//	}
+//
+//}
